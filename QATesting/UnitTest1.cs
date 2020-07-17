51b66e8124611c4e1cd3f27d619065c95b67bd26
Bogdan <bogdan.ponocko@gmail.com>
2020-07-17 15:40:57 +0200

    handling alerts

---
+++ QATesting/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics;
+using System.Threading;
+
+namespace QATesting
+{
+    [TestClass]
+    public class HandlingAlerts
+    {
+
+        IWebDriver driver;
+
+        [TestMethod]
+        public void SimpleAlert()
+
+        {
+            try
+            {
+                driver = new ChromeDriver(); 
+                driver.Navigate().GoToUrl("https://www.toolsqa.com/handling-alerts-using-selenium-webdriver/");
+                driver.Manage().Window.Maximize();
+
+                IWebElement SimpleAlertButton = driver.FindElement(By.XPath("//button[.='Simple Alert']"));
+                SimpleAlertButton.Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Debug.WriteLine("\n\n\n\n\n\n" +alert.Text + "\n\n\n\n");
+                alert.Accept();
+                driver.SwitchTo().DefaultContent();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void ConfirmAlert()
+
+        {
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://www.toolsqa.com/handling-alerts-using-selenium-webdriver/");
+                driver.Manage().Window.Maximize();
+
+                IWebElement ConfirmAlertButton = driver.FindElement(By.XPath("//button[.='Confirm Pop up']"));
+                ConfirmAlertButton.Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Debug.WriteLine("\n\n\n\n\n\n" + alert.Text + "\n\n\n\n");
+                //alert.Accept();
+                alert.Dismiss();
+                driver.SwitchTo().DefaultContent();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
